a20d1037160f09d82ff49c10940fbde5d5bc7888
Zul Ikram Musaddik Rayat <rayathossain49@gmail.com>
2025-09-19 03:56:39 +0600

    Enable minification in Vite config and add PostCSS and Tailwind CSS configuration files

--- js-local/vite.config.js
+++ js-local/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
     plugins: [react()],
     build: {
-      minify: false,
+      minify: true,
     },
   };
 });
